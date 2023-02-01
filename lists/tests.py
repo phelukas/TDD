ded63632b502c3327542d3317f2a28d411b022c4
phelukas <ppedrolukas19@gmail.com>
2023-02-01 19:08:49 -0300

    Finish cap 06

--- lists/tests.py
+++ lists/tests.py
@@ -13,16 +13,6 @@ class HomePageTest(TestCase):
         response = self.client.get('/')
         self.assertTemplateUsed(response, 'home.html')
 
-    def test_home_page_returns_correct_html(self):
-        response = self.client.get('/')
-
-        html = response.content.decode('utf8')
-        self.assertTrue(html.startswith('<html>'))
-        self.assertIn('<title>To-Do lists</title>', html)
-        self.assertTrue(html.strip().endswith('</html>'))
-
-        self.assertTemplateUsed(response, 'home.html')
-
     def test_can_save_a_POST_request(self):
         self.client.post('/', data={'item_text': 'A new list item'})
 
