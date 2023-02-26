a114c3a2c33c322704d311eb697632daf245100a
phelukas <ppedrolukas19@gmail.com>
2023-02-26 14:21:36 -0300

    Refatorar visualização de lista para lidar com novos POSTs de itens

--- lists/views.py
+++ lists/views.py
@@ -22,14 +22,10 @@ def add_item(request, list_id):
 
 def view_list(request, list_id):
     list_ = List.objects.get(id=list_id)
-    item = Item.objects.create(
-        text=request.POST['item_text'],
-        list=list_
-    )
-    try:
-        item.full_clean()
-    except ValueError:
-        list_.delete()
-        error = "Você não pode ter um item de lista vazio"
-        return render(request, 'home.html', {"error": error})
-    return redirect(f'/lists/{list_.id}/')
+    if request.method == 'POST':
+        item = Item.objects.create(
+            text=request.POST['item_text'],
+            list=list_
+        )
+        return redirect(f'/lists/{list_.id}/')
+    return render(request, 'home.html', {"error": list_})
